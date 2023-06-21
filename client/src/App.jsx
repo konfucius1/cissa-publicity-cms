c9475b5a7e372b7557497c35503ede6bcd0dcef1
Thanakon <kimsanthanakon@gmail.com>
2023-06-22 02:56:34 +1000

    feat: Set up client with Vite and Tailwind configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center h-screen text-3xl font-bold">
+        CISSA Publicity CMS
+      </h1>
+    </>
+  );
+}
