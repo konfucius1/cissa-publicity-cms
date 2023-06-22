44a144f248f252147066bcc85018556950f7b6ca
Thanakon <kimsanthanakon@gmail.com>
2023-06-22 22:25:14 +1000

    Added react router and daisyUI to project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,17 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['cmyk'],
+  },
 };
